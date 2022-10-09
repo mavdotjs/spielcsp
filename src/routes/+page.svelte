3736062c89759535d1c3b90710128247a4ebae28
maverickquill55 <VScode@toxik.cf>
2022-10-09 11:01:25 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div>
+    <object title="spiel logo" data="spiel.svg" type="image/svg+xml"></object>
+    <span>spiel.place</span>
+    <span>Chat App Coming Soon</span>
+    <span>By Consor Development</span>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+const config = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+
+  theme: {
+    extend: {
+      colors: {
+        'main': '#16C0C0',
+        'main-dark': '#0b6060'
+      }
+    },
+  },
+
+  plugins: [],
+};
+
+module.exports = config;
