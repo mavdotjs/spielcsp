7789ebad2e7cb44e2e4358aff1ab3378976c94e0
MaverickQuill <consorceo300@icloud.com>
2022-10-10 17:58:56 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
     <object title="spiel logo" data="spiel.svg" type="image/svg+xml"></object>
     <span>spiel.place</span>
     <span>Chat App Coming Soon</span>
-    <span>By Consor Development</span>
+    <span>By Consor Development (contact Chad Freeman <a href="mailto:admin@spiel.space">@admin</a>)</span>
 </div>
